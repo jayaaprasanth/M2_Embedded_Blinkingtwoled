8ed2adc7ad7facbf7b8d80f2c6b5e02930ad2209
jayaaprasanth <94215142+jayaaprasanth@users.noreply.github.com>
2021-12-02 19:27:09 +0530

    Update blinking.c

--- 3_Implementation/src/blinking.c
+++ 3_Implementation/src/blinking.c
@@ -1,1 +1,16 @@
 
+nt main(void)
+{
+	DDRB |= (1<<DDB2)|(1<<DDB3);
+	
+	while (1)
+	{
+		PORTB |=(1<<PORTB2);
+		PORTB &=~(1<<PORTB3);
+		_delay_ms(1000);
+			
+		PORTB &=~(1<<PORTB2);
+		PORTB |=(1<<PORTB3);
+		_delay_ms(1000);
+	}
+}
